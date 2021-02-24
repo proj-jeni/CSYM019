042828d02efbe8ee05b35e8a82635c952d236fee
proj-jeni <lourdujenifa@gmail.com>
2021-02-24 10:42:35 +0000

    This is week4 exercises

---
+++ Week4/Javascript/Exercise5.js
@@ -0,0 +1,9 @@
+function changecontent()
+{
+var myvariable1=document.getElementById('P1');
+var myvariable2=document.getElementById('P2');
+myvariable1.firstChild.nodeValue='Heading 2';
+myvariable2.firstChild.nodeValue='Content 2';
+
+}
+document.addEventListener('click', changecontent);
